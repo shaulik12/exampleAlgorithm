2a944a664cc7ba58cf8e9d9b8526ec8b1aecf73a
shaulik12 <shaulik@post.bgu.ac.il>
2023-06-13 17:47:37 +0300

    fixes

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ import json
 import sys
 import os
 
-def add_annotation_to_json(outputPath):
-    inputfile = os.path.join(outputPath, "input.txt")
-    input_data = json()
+def add_annotation_to_json(ioPath):
+    inputfile = os.path.join(ioPath, "input.txt")
+    input_data = {}
     with open(inputfile, 'r') as file:
         input_data = json.load(file)
     
-    outputfile = os.path.join(outputPath, "output.txt")
-    output_data = json()
+    outputfile = os.path.join(ioPath, "output.txt")
+    output_data = {}
     output_data['Annotation'] = 'worked'
     with open(outputfile, 'w') as file:
         json.dump(output_data, file, indent=4)
@@ -19,5 +19,5 @@ if __name__ == '__main__':
         print("Please provide the path string as an argument.")
         sys.exit(1)
 
-    outputPath = sys.argv[1]
-    add_annotation_to_json(outputPath)
+    ioPath = sys.argv[1]
+    add_annotation_to_json(ioPath)
