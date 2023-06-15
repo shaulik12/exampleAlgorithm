5d997a5f782c7a4b724958766cb2ee9f33a0eb65
shaulik12 <shaulik@post.bgu.ac.il>
2023-06-15 15:15:16 +0300

    fix output

--- main.py
+++ main.py
@@ -1,41 +1,57 @@
 from PIL import Image
 import pytesseract
-import base64
 import json
+import math
 import sys
 import io
 import os
-
-def getImage(ioPath):
-    inputfile = os.path.join(ioPath, "input.txt")
-    with open(inputfile, 'r') as file:
-        imageData = json.load(file)
-    imageData = base64.b64encode(imageData["DATA"])
-    imageSteam = io.BytesIO(imageData)
-    image = Image.open(imageSteam)
-    return image
 
 def OCR(ioPath):
     image = Image.open(ioPath+"/input.png")
     greyscale = image.convert('L')
     text = pytesseract.image_to_string(greyscale)
-    print(text.strip()) 
+    return text
+
+def getDimentions(ioPath):
+    image = Image.open(ioPath+"/input.png")
+    width, height = image.size
+    return width, height
+
+def createOutput(text, imageWidth, imageHeight):
+    output = {"data": []}
+    paragraphs = text.split("\n\n")
+    heightAcc = 0
+    for paragraph in paragraphs:
+        width, height = annotationSize(paragraph, imageWidth, imageHeight)
+        annotation = {}
+        annotation["content"] = paragraph
+        annotation["startX"] = 0
+        annotation["startY"] = heightAcc
+        heightAcc += height
+        annotation["endX"] = heightAcc
+        annotation["endX"] = width
+        output["data"].append(annotation)
+    return output
+
+def annotationSize(content : str, imageWidth, imageHeight):
+    length = len(content)
+    textSquareBLockLength = round(math.sqrt(length))
+    width = min(textSquareBLockLength, imageWidth)
+    height = round(length/width)
+    return width, height
     
-def add_annotation_to_json(ioPath):
-    inputfile = os.path.join(ioPath, "input.txt")
-    input_data = {}
-    with open(inputfile, 'r') as file:
-        input_data = json.load(file)
-    
+
+def writeOutput(output, ioPath):
     outputfile = os.path.join(ioPath, "output.txt")
-    output_data = {}
-    output_data['Annotation'] = 'worked'
     with open(outputfile, 'w') as file:
-        json.dump(output_data, file, indent=4)
+        json.dump(output, file, indent=4)
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
         print("Please provide the path string as an argument.")
         sys.exit(1)
     ioPath = sys.argv[1]
-    OCR(ioPath)
+    text = OCR(ioPath)
+    imageWidth, imageHeight = getDimentions(ioPath)
+    output = createOutput(text, imageWidth, imageHeight)
+    writeOutput(output, ioPath)
